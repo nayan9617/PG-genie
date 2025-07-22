346f35e8e00e5694e512746052d4f39862e1f31d
Nayan Patidar <b24cs1047@iitj.ac.in>
2025-07-22 10:34:05 +0530

    feat: Integrate Cloudinary image upload via multer in PG create and update routes
    - Set up multer as middleware for file handling
    - Implemented uploadOnCloudinary utility function
    - Enabled image uploads in createPg and updatePg controllers

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,21 +1,32 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+import fs from "fs";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: 'PGBuddy',
-        allowedFormats: ['jpeg', 'png', 'jpg']
-    }
-})
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
 
-module.exports = {
-    cloudinary,
-    storage
-}
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File uploaded Successfully, ", response.url);
+
+    fs.unlinkSync(localFilePath);
+
+    return {
+    url: response.secure_url,   // this is tentative as it depends on response although it can be tested through postman
+    filename: response.public_id
+    };
+  } catch (error) {
+    fs.unlinkSync(localFilePath);  // remove the locally saved temporary file as the upload operation got failed!!
+    throw error;
+  }
+};
+
+export { uploadOnCloudinary }
