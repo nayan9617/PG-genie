b23419d6d11284fd2b474788a063feac8e66506f
Nihesh Rachakonda <niheshr03@gmail.com>
2025-07-25 20:05:15 +0530

    added nsfw by using sightengine #13

--- routes/PG.js
+++ routes/PG.js
@@ -1,27 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const catchAsync = require('../utils/catchAsync');
-const PG = require('../controllers/PG.js');
-const multer = require('multer');
-const { storage } = require('../cloudinary');
+const catchAsync = require("../utils/catchAsync");
+const PG = require("../controllers/PG.js");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
 const upload = multer({ storage });
+const {
+  tempUpload,
+  verifyImageContent,
+  cleanupTempFiles,
+} = require("../middleware/imageVerification");
 
-const { isLoggedIn, validatePG, isAuthor } = require('../middleware.js');
+const { isLoggedIn, validatePG, isAuthor } = require("../middleware.js");
 
-router.route('/')
-    .get(catchAsync(PG.index))
-    // .post(isLoggedIn, upload.array('image'), catchAsync(PG.createPg))
-    .post(isLoggedIn, upload.array('image'), validatePG, catchAsync(PG.createPg))
+router
+  .route("/")
+  .get(catchAsync(PG.index))
+  // Replace the direct upload with our verification pipeline
+  .post(
+    isLoggedIn,
+    tempUpload.array("image"),
+    catchAsync(verifyImageContent),
+    catchAsync(PG.uploadToCloudinary),
+    validatePG,
+    catchAsync(PG.createPg)
+  );
 
-router.get('/new', isLoggedIn, PG.renderNewForm)
+router.get("/new", isLoggedIn, PG.renderNewForm);
 
-router.get('/search', PG.searchPg);
+router.get("/search", PG.searchPg);
 
-router.route('/:id')
-    .get(catchAsync(PG.showPg))
-    .put(isLoggedIn, isAuthor, upload.array('image'), validatePG, catchAsync(PG.updatePg))
-    .delete(isLoggedIn, isAuthor, catchAsync(PG.deletePg))
+router
+  .route("/:id")
+  .get(catchAsync(PG.showPg))
+  // Update the edit route as well
+  .put(
+    isLoggedIn,
+    isAuthor,
+    tempUpload.array("image"),
+    catchAsync(verifyImageContent),
+    catchAsync(PG.uploadToCloudinary),
+    validatePG,
+    catchAsync(PG.updatePg)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(PG.deletePg));
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(PG.renderEditForm))
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(PG.renderEditForm));
 
 module.exports = router;
